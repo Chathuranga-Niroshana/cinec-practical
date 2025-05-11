14c44c8a7b26bff476edef7113fb31eddf2daf9c
Chathuranga Niroshana <134888303+Chathuranga-Niroshana@users.noreply.github.com>
2025-05-11 16:09:52 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const resultField = document.getElementById('result');
+
+function appendValue(value) {
+  resultField.value += value;
+}
+
+function clearScreen() {
+  resultField.value = '';
+}
+
+function deleteLast() {
+  resultField.value = resultField.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    resultField.value = eval(resultField.value);
+  } catch (error) {
+    alert('Invalid expression');
+  }
+}
